519912770d86c2dbc5597726078046e19be2f266
Pratik <survasepratik1304@gmail.com>
2025-12-19 19:26:47 +0530

    auth done

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+
 import { getUser, getUsers } from "../controllers/users.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const router=Router();
 
 router.get("/",getUsers)
-router.get("/:id",getUser)
+router.get("/:id",authorize,getUser)
 router.post("/",(req,res)=>{})    //add new user
 router.put("/:id",(req,res)=>{})       //update user
 router.delete("/:id",(req,res)=>{})
